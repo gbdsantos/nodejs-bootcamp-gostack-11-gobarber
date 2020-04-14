64d0dfc136dfe0b774e87f67deaf5617471abea8
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-14 17:59:49 -0300

    :star: feat(backend): add appointment route
    - Create endpoint of the route.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
+routes.use('/appointments', appointmentsRouter);
+
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  return response.json({ message: '🚀 Server has started on port 3333!' });
 });
 
 export default routes;
