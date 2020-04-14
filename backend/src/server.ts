1633ae5683172201699bd1eadc62e98b05559091
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-14 12:57:16 -0300

    :shirt: config(backend): more eslint configure for typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import routes from './routes/';
+import routes from './routes';
 
 const app = express();
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server has started on port 3333!');
