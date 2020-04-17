ffaf1ef549596586211649f7111ede87b1291574
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-16 22:21:54 -0300

    :heavy_plus_sign: update(backend): modify migrations

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
 
 @Entity('appointments')
 class Appointment {
@@ -6,10 +12,16 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
