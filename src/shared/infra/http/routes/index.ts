bceead24292679d33bfdc3158ec40aa0afa92cb0
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-09-15 22:48:08 -0300

    :pencil: update(backend): list of providers

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,18 +1,20 @@
 import { Router } from 'express';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
-import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-routes.use('/users', usersRouter);
 routes.use('/password', passwordRouter);
-routes.use('/sessions', sessionsRouter);
 routes.use('/profile', profileRouter);
+routes.use('/providers', providersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/users', usersRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: '🚀 Server has started on port 3333!' });
