9d760027cce34c4593875c90661c15119e6fbca0
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-09-17 14:51:16 -0300

    :pencil: update(backend): list available hours

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,9 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findAllInDayFromProvider(data: IFindAllInDayFromProviderDTO): Promise<Appointment[]>;
   findAllInMonthFromProvider(data: IFindAllInMonthFromProviderDTO): Promise<Appointment[]>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,14 +1,26 @@
 import { uuid } from 'uuidv4';
-import { isEqual, getMonth, getYear } from 'date-fns';
+import { isEqual, getDate, getMonth, getYear } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
+
+  public async findAllInDayFromProvider({ provider_id, day, month, year }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(appointment =>
+      appointment.provider_id === provider_id &&
+      getDate(appointment.date) === day &&
+      getMonth(appointment.date) + 1 === month &&
+      getYear(appointment.date) == year,
+    );
+
+    return appointments;
+  }
 
   public async findAllInMonthFromProvider({ provider_id, month, year }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
     const appointments = this.appointments.filter(appointment =>
