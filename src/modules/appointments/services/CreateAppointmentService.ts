c7943018e17f5266eaaedded9353873b5d17c87a
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-09-17 17:06:14 -0300

    :pencil: update(backend): appointment rules

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { getHours, isBefore, startOfHour } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -28,6 +28,21 @@ class CreateAppointmentService {
 
   public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    /** 💡 The user cannot schedule an appointment that has passed **/
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date.")
+    }
+
+    /** 💡 The user cannot schedule appointment with himself **/
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.")
+    }
+
+    /** 💡 Appointments must be available between 8 am and 6 pm (First at 8 am, last at 5 pm) **/
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError("You can only create appointments between 8am and 5pm.");
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
