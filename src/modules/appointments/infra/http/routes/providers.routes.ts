670a4fb0233af70e44d2e337fbd064179aa4c661
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-09-17 17:53:37 -0300

    :pencil: update(backend): routes and controllers
    - ListProviderDayAvailabilityController;
    - ListProviderMonthAvailabilityController.

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
+import LisProviderDayAvailability from '@modules/appointments/infra/http/controllers/ListProviderDayAvailabilityController';
+import LisProviderMonthAvailability from '@modules/appointments/infra/http/controllers/ListProviderMonthAvailabilityController';
 import ProvidersController from '../controllers/ProvidersController';
 
 const providersRouter = Router();
@@ -9,5 +11,7 @@ const providersRouter = Router();
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', ProvidersController.index);
+providersRouter.get('/:provider_id/month-availability', LisProviderDayAvailability.index);
+providersRouter.get('/:provider_id/day-availability', LisProviderMonthAvailability.index);
 
 export default providersRouter;
