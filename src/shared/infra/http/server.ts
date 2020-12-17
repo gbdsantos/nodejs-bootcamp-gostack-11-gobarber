04d83e0ea1ca1655604e37cddd4d8c087030e149
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-12-17 19:58:57 -0300

    :star: feat(backend): implement middleware that prevent brute force attack

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,20 +3,26 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import { errors } from 'celebrate';
-import cors from 'cors';
+
 import 'express-async-errors';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 import uploadConfig from '@config/upload';
-import AppError from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm';
 
+// Middlewares Imports
+import cors from 'cors';
+import rateLimiter from './middlewares/rateLimiter';
+
 const app = express();
 
+// Middlewares
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
