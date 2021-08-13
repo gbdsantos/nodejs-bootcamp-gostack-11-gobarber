5b62fb3c0d36749cd7ef1ff34359aee3a74a36ab
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-08-13 15:50:54 -0300

    :recycle: refactor(backend): remove rate limit for api image requests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -22,10 +22,11 @@ import rateLimiter from './middlewares/rateLimiter';
 const app = express();
 
 // Middlewares
-app.use(rateLimiter);
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
